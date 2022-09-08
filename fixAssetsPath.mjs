2e558a7a60f86804e72568e02d38cece72b0eb4c
alonbalbuena <alonbalbuena@gmail.com>
2022-09-09 00:26:39 +0200

    fix(assets): make api content path work

--- fixAssetsPath.mjs
+++ fixAssetsPath.mjs
@@ -1,5 +1,5 @@
 import * as fs from 'fs';
 
 const content = fs.readFileSync('./dist/index.html', 'utf8');
-const fixedContent = content.replace(/\/_nuxt\//g,'/personal/_nuxt/')
+const fixedContent = content.replace(/(\/(?:_nuxt|api)\/)/g,'/personal$1')
 fs.writeFileSync('./dist/index.html', fixedContent)
