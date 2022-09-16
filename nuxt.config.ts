b292278b266a128b6ba6bbfd80cd3574e06dd47c
alonbalbuena <alonbalbuena@gmail.com>
2022-09-16 23:46:37 +0200

    feat(nuxt): allow document driven

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,3 @@
-import { defineNuxtConfig } from 'nuxt';
-
-// https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
   target: 'static', router: {
     base: '/personal/_nuxt/'
@@ -8,4 +5,7 @@ export default defineNuxtConfig({
   modules: [
     '@nuxt/content'
   ],
+  content: {
+    documentDriven: true
+  }
 });
